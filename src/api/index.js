e45054d978c51f76e09ebba3d54c6352b7021a68
Carlos <krloscjr@gmail.com>
2021-03-28 12:08:08 -0500

    fix pages

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,8 @@ const api_Key = config.APIkey
 const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&page=:page&country=:pais&api_key=${api_Key}&format=json`;
 
 export default function getArtist(pais, page) {
-
   let urls = URL.replace(':page', page)
   let url = urls.replace(':pais', pais)
-  console.log(url)
   return fetch(url)
     .then(res => res.json())
     .then(json => json.topartists.artist)
